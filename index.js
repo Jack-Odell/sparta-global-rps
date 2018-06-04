64747cbbce7cfe0a305b775c56956a7dd3171252
Tech-A48 <tech-a48@Tech-A48s-MacBook-Air.local>
2018-06-04 12:44:19 +0100

    Updated README.md. Alerts now show what the computer has chosen. Removed all console logs.

--- index.js
+++ index.js
@@ -1,68 +1,96 @@
-var play = prompt("What's your move?  --  (R)ock, (P)aper or (S)cissors");
-var x;
+var playerChoice;
+var cpuChoice;
+var i = 0;
+var cpuScore = 0;
+var playerScore = 0;
+var winCondition = 3;
+var gameWon = false;
 
-if (play === "r") {
-  x = "rock";
-  console.log("Player " + x);
-}
-else if (play === "p") {
-  x = "paper";
-  console.log("player " + x);
-}
-else if (play === "s") {
-  x = "scissors";
-  console.log("player " + x);
+function playerWinRound () {
+    alert("Player Wins!");
+    playerScore++;
 }
 
-
-var cpuChance = Math.random();
-var y;
-
-if (cpuChance < 0.34) {
-  y = "rock";
-  console.log("cpu rock");
-}
-else if (cpuChance <= 0.67) {
-  y = "paper";
-  console.log("cpu paper");
-}
-else {
-  y = "scissors";
-  console.log("cpu scissors");
+function cpuWinRound () {
+    alert("Computer Wins...");
+    cpuScore++;
 }
 
-
-
-
-var match = function (x, y) {
-  if (x === y) {
-    return "It's a tie...";
+function winTheGame () {
+  if (playerScore == winCondition) {
+    playerScore = 0;
+    cpuScore = 0;
+    alert("Player wins the game!");
+    gameWon = true;
   }
-  if (x === "rock") {
-    if (y === "scissors") {
-      return "Player Wins!";
-    }
-    else {
-      return "Computer wins...";
-    }
-  }
-  if (x === "paper") {
-    if (y === "scissors") {
-      return "Player Wins!";
-    }
-    else {
-      return "Computer wins...";
-    }
-  }
-  if (x === "scissors") {
-    if (y === "paper") {
-      return "Player Wins!";
-    }
-    else {
-      return "Computer wins...";
-    }
+  else if (cpuScore == winCondition) {
+    playerScore = 0;
+    cpuScore = 0;
+    alert("Computer wins the game...")
+    gameWon = true;
   }
 }
-// playerChoice();
-// cpuChoice();
-var results = match(x, y);
+
+while (gameWon === false) {
+  var gameWon = false;
+  var cpuChance = Math.random();
+  var play = prompt("What's your move?  --  (R)ock, (P)aper or (S)cissors");
+  if (play === "r") {
+    playerChoice = "rock";
+  }
+  else if (play === "p") {
+    playerChoice = "paper";
+  }
+  else if (play === "s") {
+    playerChoice = "scissors";
+  }
+
+
+  if (cpuChance < 0.34) {
+    cpuChoice = "rock";
+    alert("Computer chose " + cpuChoice);
+  }
+  else if (cpuChance <= 0.67) {
+    cpuChoice = "paper";
+    alert("Computer chose " + cpuChoice);
+  }
+  else {
+    cpuChoice = "scissors";
+    alert("Computer chose " + cpuChoice);
+  }
+
+  var match = function (playerChoice, cpuChoice) {
+    if (playerChoice === cpuChoice) {
+      alert("It's a tie...");
+    }
+    else if (playerChoice === "rock") {
+      if (cpuChoice === "scissors") {
+        playerWinRound();
+      }
+      else {
+        cpuWinRound();
+      }
+    }
+
+    else if (playerChoice === "paper") {
+      if (cpuChoice === "rock") {
+        playerWinRound();
+      }
+      else {
+        cpuWinRound();
+      }
+    }
+    else if (playerChoice === "scissors") {
+      if (cpuChoice === "paper") {
+        playerWinRound();
+      }
+      else {
+        cpuWinRound();
+      }
+    }
+  }
+
+  match(playerChoice, cpuChoice);
+  i++;
+  winTheGame();
+}
